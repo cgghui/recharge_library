88f7cd02f1c5f4ced7494d845dab3483f25507a8
gchen <gchen3544@gmail.com>
2022-08-22 09:52:41 +0800

    优化

--- sys/db_model.go
+++ sys/db_model.go
@@ -105,7 +105,7 @@ type MerchantGoods struct {
 	RelatedCode  string
 	Enable       YN
 	CreatedAt    Time
-	RelatedList  []MerchantGoodsRelated `gorm:"foreignKey:code;references:sku_code"`
+	RelatedList  []MerchantGoodsRelated `gorm:"foreignKey:code;references:related_code"`
 }
 
 type MerchantGoodsRelated struct {
